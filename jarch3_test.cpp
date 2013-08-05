ad618a446cedc8ed96a1d1971a1154326522e436
Jonathan Campbell <jonathan@castus.tv>
2013-08-05 00:15:00 -0700

    more work. good job.

--- jarch3_test.cpp
+++ jarch3_test.cpp
@@ -272,6 +272,65 @@ size_t Jarch3Device::read_buffer_data_length() {
 	return data_length;
 }
 
+static const char *scsi_keys[16] = {
+	"No Sense",		/* 0x0 */
+	"Soft Error",		/* 0x1 */
+	"Not Ready",		/* 0x2 */
+	"Medium Error",		/* 0x3 */
+
+	"Hardware Error",	/* 0x4 */
+	"Illegal Request",	/* 0x5 */
+	"Unit Attention",	/* 0x6 */
+	"Write Protect",	/* 0x7 */
+
+	"",			/* 0x8 */
+	"",			/* 0x9 */
+	"",			/* 0xA */
+	"Aborted Command",	/* 0xB */
+
+	"",			/* 0xC */
+	"",			/* 0xD */
+	"",			/* 0xE */
+	""			/* 0xF */
+};
+
+static const char *scsi_asc(unsigned char key,unsigned char asc,unsigned char ascq) {
+	switch (key) {
+		case 0x0:
+			switch (asc) {
+				case 0x00:	return "No error";
+				case 0x5D:	return "No sense - PFA threshold reached";
+			}
+			break;
+		case 0x1:
+			switch ((asc<<8)+ascq) {
+				case 0x0100:	return "Recovered Write error - no index";
+				case 0x0200:	return "Recovered no seek completion";
+				/* TODO: The rest */
+			}
+			break;
+		case 0x2:
+			switch ((asc<<8)+ascq) {
+				case 0x0400:	return "Not Ready - Cause not reportable.";
+				case 0x0401:	return "Not Ready - becoming ready";
+				case 0x0402:	return "Not Ready - need initialise command (start unit)";
+				/* TODO: The rest */
+			}
+			break;
+		case 0x5:
+			switch ((asc<<8)+ascq) {
+				case 0x5302:	return "Illegal Request - medium removal prevented";
+				/* TODO: The rest */
+			}
+			break;
+
+		default:
+			break;
+	};
+
+	return "?";
+}
+
 void Jarch3Device::dump_sense(FILE *fp) {
 	int x;
 
@@ -282,8 +341,17 @@ void Jarch3Device::dump_sense(FILE *fp) {
 	}
 
 	fprintf(fp,"Sense data[%u]: ",(unsigned int)sense_length);
-	for (x=0;x < (int)sense_length;x++) fprintf(fp,"%02x ",sense[x]);
+	for (x=0;x < (int)sense_length;x++) {
+		if ((x&3) == 3) fprintf(fp," ");
+		fprintf(fp,"%02x ",sense[x]);
+	}
 	fprintf(fp,"\n");
+
+	if (sense_length > 2) {
+		fprintf(fp,"     Which means: Key=%s  ASC=%s\n",
+			scsi_keys[sense[2]&0xF],
+			scsi_asc(sense[2]&0xF,sense[12],sense[13]));
+	}
 }
 
 int Jarch3Device::do_scsi(int UNUSED direction,size_t UNUSED data_length) {
