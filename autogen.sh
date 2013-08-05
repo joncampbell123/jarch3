fb75dba30937ae8f3232fd366338cc5427872245
Jonathan Campbell <jonathan@castus.tv>
2013-08-04 19:43:59 -0700

    more

---
+++ autogen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo touch NEWS README AUTHORS ChangeLog INSTALL COPYING
+touch NEWS README AUTHORS ChangeLog INSTALL COPYING
+
+echo autoreconf --verbose --install --symlink --warnings=all
+exec autoreconf --verbose --install --symlink --warnings=all
+
---
+++ jarch3_test.cpp
@@ -0,0 +1,18 @@
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <scsi/sg.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc,char **argv) {
+	return 0;
+}
+
