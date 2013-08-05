1c7962be9a3a79d496ee3179e1351c7418a8cef4
Jonathan Campbell <jonathan@castus.tv>
2013-08-04 19:45:41 -0700

    more

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,8 @@
 echo touch NEWS README AUTHORS ChangeLog INSTALL COPYING
 touch NEWS README AUTHORS ChangeLog INSTALL COPYING
 
+mkdir -p m4
+
 echo autoreconf --verbose --install --symlink --warnings=all
 exec autoreconf --verbose --install --symlink --warnings=all
-
-mkdir -p m4
 
